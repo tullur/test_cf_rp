0c8ed853223b1cc70881ca92c4f71a3108516524
Cirill Ax <akson.rill@gmail.com>
2020-11-05 21:29:50 +0200

    Add age to Person class and create student class Add Gemfile and create module

--- main.rb
+++ main.rb
@@ -1,14 +1,25 @@
 class Person
-  def initialize(name, surname)
+  def initialize(name, surname, age)
     @name = name
     @surname = surname
+    @age = age
   end
 
   def info
-    puts "#{@name} #{@surname}"
+    puts "Name: #{@name} \nSurname: #{@surname} \nAge: #{@age}"
+  end
+end
+
+class Student < Person
+  def initialize(name, surname, age, grade)
+    super(name, surname, age)
+    @grade = grade
   end
 end
 
 
-p = Person.new('Test', 'Test')
+p = Person.new('Test', 'Test', 1_000_000)
 p.info
+
+s = Student.new('Student', 'Student', 2_000_000, 'A')
+s.info
